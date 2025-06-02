bc75d501d28d357cef1769ebf96b904d1bc81d41
Raoul Kaleba <raouldev@outlook.com>
2025-06-01 19:09:20 -0500

    Added calculate function

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,30 @@ enum Operator {
     Subtract,
     Multiply,
     Divide,
-    Power
+    Power,
 }
+
+// Calculate function
+fn calculate (operator: Operator, numbers: (f64, f64)) -> f64 {
+    match operator { 
+        Operator::Add => {
+            numbers.0 + numbers.1
+        }
+        Operator::Subtract => {
+            numbers.0 - numbers.1
+        }
+        Operator::Multiply => {
+            numbers.0 * numbers.1
+        }
+        Operator::Divide => {
+            numbers.0 / numbers.1
+        }
+        Operator::Power => {
+            numbers.0.powf(numbers.1)
+        }
+    }
+}
+
 
 fn main() {
     println!("Welcome to CLI Calculator")
