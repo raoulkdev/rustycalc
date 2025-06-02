e17aee5839b91e4c9d4ee9c200ec1968daee98a0
Raoul Kaleba <raouldev@outlook.com>
2025-06-01 19:20:08 -0500

    Removed operator enum and setup operator input

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
-// Operator enum
-enum Operator {
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
-    Power,
-}
+use std::io::{stdin, stdout, Write};
 
 // Calculate function
-fn calculate (operator: Operator, numbers: (f64, f64)) -> f64 {
+fn calculate (operator: char, numbers: (f64, f64)) -> f64 {
     match operator { 
-        Operator::Add => {
+        '+' => {
             numbers.0 + numbers.1
         }
-        Operator::Subtract => {
+        '-' => {
             numbers.0 - numbers.1
         }
-        Operator::Multiply => {
+        '*' => {
             numbers.0 * numbers.1
         }
-        Operator::Divide => {
+        '/' => {
             numbers.0 / numbers.1
         }
-        Operator::Power => {
+        '^' => {
             numbers.0.powf(numbers.1)
         }
+        _ =>{
+            0.0
+        }
     }
+}
+
+// Handle input
+fn input_handling(){
+    let mut operator = ' ';
+    let mut numbers = (0.0, 0.0);
+    let mut operator_input = String::new();
+    let mut number_1_input = String::new();
+    let mut number_2_input = String::new();
+    
+    // Operator
+    print!("Operator (+, ): ");
+    stdout().flush().unwrap();
+    stdin()
+        .read_line(&mut operator_input)
+        .expect("[ERR] => Failed to read operator");
+    operator = operator_input.trim().chars().next().unwrap();
 }
 
 
